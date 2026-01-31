5704c5df21f1fc0c1c12429d83bbbc062a3ebafa
mohamed1160 <mohamed.ahmed102006@gmail.com>
2026-01-31 02:35:56 +0200

    add a 3d model with switch size and color and make some lights

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,17 +1,23 @@
-import React from "react";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
-import useMacbookStore from "../Store/index.js";
+// import {useMacbookStore} from "../Store/index.js";
 
 import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import { useMediaQuery } from "react-responsive";
+import { useMacbookStore } from "../Store/index.js";
+
 const ProductViewer = () => {
-    const { color, setColor, scal, setScal } = useMacbookStore();
+    const { color, setColor, scale, setScale } = useMacbookStore();
+    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>{" "}
             <div className="controls">
                 <p className="info">
-                    MacBook Pro {scal}" in {color}{" "}
+                    Macbook pro available in 14" & 16" in space gray and dark colors{" "}
                 </p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control flex gap-2">
@@ -19,18 +25,20 @@ const ProductViewer = () => {
                         <div onClick={() => setColor("#2c2e2c")} className={clsx("bg-neutral-900", color == "#2c2e2c" && "active")}></div>{" "}
                     </div>
                     <div className="size-control ">
-                        <div onClick={() => setScal(0.06)} className={clsx(scal == 0.06 ? "bg-white text-black" : "text-white bg-transparent")}>
+                        <div onClick={() => setScale(0.06)} className={clsx(scale == 0.06 ? "bg-white text-black" : "text-white bg-transparent")}>
                             <p>14"</p>
                         </div>
-                        <div onClick={() => setScal(0.08)} className={clsx(scal == 0.08 ? "bg-white text-black" : "text-white bg-transparent")}>
+                        <div onClick={() => setScale(0.08)} className={clsx(scale == 0.08 ? "bg-white text-black" : "text-white bg-transparent")}>
                             <p>16"</p>
                         </div>
                     </div>
                 </div>
             </div>
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
-                {/* <MacbookModel14 scal={0.06} position={[-1, 0, 0]} /> */}
-                <OrbitControls enableZoom={false} />
+                <StudioLights />
+
+                <ModelSwitcher
+                    scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
             </Canvas>
         </section>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ProductViewer from '../components/ProductViewer'
+import  gsap  from 'gsap' 
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
